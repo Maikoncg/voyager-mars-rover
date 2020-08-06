8d6ea0f8c83c52bdb421deb3db76faf0537f02e2
Cassiano Mangold <cassiano@voyagerportal.com>
2020-08-06 16:04:45 -0300

    feat(Voyage Mars Rover Test):

---
+++ source/voyager-mars-front/src/api/company.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import { apiUrl } from '.';
+
+export default {
+  async fetch(params) {
+    return axios.get(`${apiUrl}/companies`, { params });
+  },
+  async create(params) {
+    return axios.post(`${apiUrl}/companies`, params);
+  },
+  async update(id, params) {
+    return axios.put(`${apiUrl}/companies/${id}`, params);
+  },
+  async delete(id) {
+    return axios.delete(`${apiUrl}/companies/${id}`);
+  },
+  async show(id) {
+    return axios.get(`${apiUrl}/companies/${id}`);
+  },
+};
---
+++ source/voyager-mars-front/src/plugins/util.js
@@ -0,0 +1,17 @@
+/**
+ * Method to get js default modules
+ * @param files
+ * @param isArray
+ * @returns {*}
+ */
+export function getJsModules(files, isArray = false) {
+  const modules = isArray ? [] : {};
+  files.keys().forEach((key) => {
+    if (key === './index.js') return;
+    if (isArray) files(key).default.map(r => modules.push(r));
+    else modules[key.replace(/(\.\/|\.js)/g, '')] = files(key).default;
+  });
+  return modules;
+};
+
+
